7a055773d24292643a8e0ed04bc91ee1512bb445
naim <96814060+naim514@users.noreply.github.com>
2023-03-21 19:04:27 +0100

    Mise à jour pxt.json, main.blocks, main.ts, custom.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
-basic.forever(function () {
-	
+IoT_Bit_naim.on_HTTP_recevid(function (HTTP_Status_Code, Data) {
+    basic.showString("Hello!")
 })
+basic.showIcon(IconNames.Square)
+IoT_Bit_naim.initWIFI(SerialPin.P8, SerialPin.P12, BaudRate.BaudRate115200)
+IoT_Bit_naim.connectWifi("A12", "ubuy9109")
+IoT_Bit_naim.connectThingSpeak()
+IoT_Bit_naim.sendGenericHttp(
+IoT_Bit_naim.httpMethod.GET,
+"https://api.thingspeak.com/channels/1028055/feeds.json?api_key=1IFCE89OOGQ27NXI&results=1",
+""
+)
